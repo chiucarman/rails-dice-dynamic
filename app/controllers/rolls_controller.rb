d12a54623bac20951822b721dea503ac5ad60496
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-26 20:38:58 +0000

    flexible route working!

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -2,4 +2,20 @@ class RollsController < ApplicationController
   def home
     render({ :template => "dice_templates/homepage"})
   end
+
+  def flexible
+    @num_dice = params.fetch("num_dice").to_i
+
+    @num_sides = params.fetch("num_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+
+      @rolls.push(die)
+    end
+    
+    render({ :template => "dice_templates/flexible"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "rolls", :action => "home" })
 
+  get("/dice/:num_dice/:num_sides", { :controller => "rolls", :action => "flexible"})
+
 end
